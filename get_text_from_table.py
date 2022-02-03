f5b80757a244d70ee90c7e741df99b47d0684a00
Roanna Victorio <roannav@users.noreply.github.com>
2022-02-02 17:06:20 -0800

    handle ties in ranking

--- get_text_from_table.py
+++ get_text_from_table.py
@@ -45,47 +45,78 @@ def get_text_from_table(soup,
     table_data = tab.find_all("tr")[1:]
 
     for tr in table_data:
-        row = []
-        tds = tr.find_all("td")
-        row.append(tds[0].getText())
-
-        if add_song_url_column:
-            link = None
-            if tds[1].find("a"):
-                link = tds[1].find("a").attrs["href"] 
-            row.append(link)
-
-        #row_num = int(tds[0].getText())   # for debugging
-
-        '''
-        Sometimes there is an extra <sup> tag, as below.  Remove it.
-        <td>"<a href="/wiki/Babe_(Styx_song)" title="Babe (Styx song)">Babe</a>"
-        <sup id="cite_ref-Note1979_3-1" class="reference">
-            <a href="#cite_note-Note1979-3">&#91;Notes 1&#93;</a>
-        </sup>
-        </td>
-
-        Sometimes the title (or part of the title is not in an <a> tag.
-        So we can NOT just select the text from the <a> tag.
-        Instead remove the <sup> tag.
-        '''
-        if tds[1].sup:             # if there is a <sup> tag in tds[1],
-            tds[1].sup.extract()   # remove it
-
-        # The second td has an extra pair of quotes which encloses the title
-        # like this...
-        # <td>"<a href="/wiki/Call_Me_(Blondie_song)" 
-        # title="Call Me (Blondie song)">Call Me</a>"</td>
-        # Remove the extra quotes via the slice [1:-1].
-        # Also extract() can add extra whitespace, so strip the whitespace.
-        row.append(tds[1].getText(strip=True)[1:-1])
-
-        # The third td ends with an extra '\n' at the end.  Remove it.
-        row.append(tds[2].getText()[:-1])
-
+        row = build_row(tr, add_song_url_column)
         data.append(row)
     
     return data, cols
+
+
+def has_rowspan( td):
+    return td and ("rowspan" in td.attrs)
+
+
+# returns the row, as a list, but with just text (no html) of each td cell
+def build_row(tr, add_song_url_column):
+    row = []
+    tds = tr.find_all("td")
+
+    # This is the index in the list of tds we are looking at. 
+    # This may be different than which column of the main table.
+    index = 0  
+
+    if has_rowspan(tds[0]):
+        # sometimes the first td contains something like "50 (Tie)" 
+        # and has rowspan="2"
+        print("There's a tie!")
+        # This will offset the index of the tds in the next row.
+
+    if len(tds) == 2:
+        # If there are only 2 columns in this row,
+        # it's because there was a rowspan in the first td of previous row.
+        # This happens when there's a tie in the rankings.
+        prev_row = tr.find_previous_sibling('tr')    
+        # FIXME: This assumes only a 2-way tie.
+        rank = prev_row.find("td").getText()
+    else:
+        rank = tds[index].getText()
+        index += 1
+
+    row.append(rank)
+
+    if add_song_url_column:
+        link = None
+        if tds[index].find("a"):
+            link = tds[index].find("a").attrs["href"] 
+        row.append(link)
+
+
+    '''
+    Sometimes there is an extra <sup> tag, as below.  Remove it.
+    <td>"<a href="/wiki/Babe_(Styx_song)" title="Babe (Styx song)">Babe</a>"
+    <sup id="cite_ref-Note1979_3-1" class="reference">
+        <a href="#cite_note-Note1979-3">&#91;Notes 1&#93;</a>
+    </sup>
+    </td>
+
+    Sometimes the title (or part of the title is not in an <a> tag.
+    So we can NOT just select the text from the <a> tag.
+    Instead remove the <sup> tag.
+    '''
+    if tds[index].sup:             # if there is a <sup> tag in tds[index],
+        tds[index].sup.extract()   # remove it
+
+    # The second td has an extra pair of quotes which encloses the title
+    # like this...
+    # <td>"<a href="/wiki/Call_Me_(Blondie_song)" 
+    # title="Call Me (Blondie song)">Call Me</a>"</td>
+    # Remove the extra quotes via the slice [1:-1].
+    # Also extract() can add extra whitespace, so strip the whitespace.
+    row.append(tds[index].getText(strip=True)[index:-1])
+    index += 1
+
+    # The third td ends with an extra '\n' at the end.  Remove it.
+    row.append(tds[index].getText()[:-1])
+    return row
 
 
 def convert_table_to_csv( data, cols, csv_filename):
@@ -115,5 +146,11 @@ def run_tests():
     # "table.wikitable", ".wikitable", "table.sortable"
     data, cols = get_text_from_table(soup, False, "table.wikitable")
     convert_table_to_csv( data, cols, 'output3.csv')
+
+    html_filename = 'html/Billboard_Top50_in_1958.html'
+    soup = bs4.BeautifulSoup(open(html_filename), features='html.parser')
+    data, cols = get_text_from_table(soup, True)
+    convert_table_to_csv( data, cols, 'output4.csv')
+
 
 #run_tests()
